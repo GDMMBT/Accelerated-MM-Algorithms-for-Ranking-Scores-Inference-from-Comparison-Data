bdc62951e39792948ddf9457d24c10f8f45f5b50
GDMMBT <56114367+GDMMBT@users.noreply.github.com>
2020-12-20 03:27:16 +0000

    Update MMandAccMMbt.R

--- code/MMandAccMMbt.R
+++ code/MMandAccMMbt.R
@@ -30,6 +30,38 @@ Bayesian.MM=function(init,tiny,alpha,beta,w){
   return(list(Iterationmatrix=iterationmatrix))
 }
 
+##################################################################################
+###MM algorithm for Bradley-Terry with normalisation at each step such that \sum_{i=1}^n exp(w_i)=1.
+
+Bayesian.MM.Norm=function(init,tiny,alpha,beta,w){
+  n=dim(w)[1]
+  iterationmatrix=matrix(0,100000,n)
+  i=0
+  x1=init
+  x0=x1+rep(10*tiny,length(x1))
+  u=c()
+  l=c()
+  while(sum(abs(x0-x1)>tiny)>0 && i<3000) {
+    x0=x1
+    for(j in 1:n){
+      for(k in 1:n){
+        if(k != j){
+          u[k]=w[j,k]
+          l[k]=(w[j,k]+w[k,j])/(exp(x0[j])+exp(x0[k]))
+        }
+        else{u[k]=0
+        l[k]=0
+        }
+      }
+      x1[j]=log((alpha-1+sum(u))/(beta+sum(l)))
+    }
+    i=i+1
+    x1=log(exp(x1)/sum(exp(x1)))  
+    iterationmatrix[i,]=x1
+  }
+  iterationmatrix=iterationmatrix[1:i,1:n]
+  return(list(Iterationmatrix=iterationmatrix))
+}
 
 ##################################################################################
 ###AccMM algorithm for Bradley-Terry. w is the winning matrix.
@@ -89,6 +121,21 @@ dim(ImatrixMM.1)
 dim(ImatrixMM.01)
 dim(ImatrixMM0)
 
+##################################################################################
+####For GIFGIF Sample datasets
+ImatrixMM.Norm=Bayesian.MM.Norm(init,tiny,11,10,w)$Iterationmatrix
+ImatrixMM.Norm.21=Bayesian.MM.Norm(init,tiny,2,1,w)$Iterationmatrix
+ImatrixMM.Norm.1=Bayesian.MM.Norm(init,tiny,1.1,0.1,w)$Iterationmatrix
+ImatrixMM.Norm.01=Bayesian.MM.Norm(init,tiny,1.01,0.01,w)$Iterationmatrix
+ImatrixMM.Norm0=Bayesian.MM.Norm(init,tiny,1,0,w)$Iterationmatrix
+
+dim(ImatrixMM.Norm)
+dim(ImatrixMM.Norm.21)
+dim(ImatrixMM.Norm.1)
+dim(ImatrixMM.Norm.01)
+dim(ImatrixMM.Norm0)
+
+##################################################################################
 AccImatrixMM=Acc.Bayesian.MM(init,tiny,11,10,w)$Iterationmatrix
 AccImatrixMM.21=Acc.Bayesian.MM(init,tiny,2,1,w)$Iterationmatrix
 AccImatrixMM.1=Acc.Bayesian.MM(init,tiny,1.1,0.1,w)$Iterationmatrix
